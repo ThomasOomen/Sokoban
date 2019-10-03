3e6413f7791193e51efcb72bf018b78c10c9b80b
MitchVerweerden <mverweer@avans.nl>
2019-10-03 13:58:52 +0200

    Grootte doolhof berekenen + vullen van 2d array

--- Sokoban/Parser.cs
+++ Sokoban/Parser.cs
@@ -6,11 +6,13 @@ using System.Text;
 namespace Sokoban
 {
     public class Parser
-        {
+    {
         private string _path;
         private FileStream _fileStream;
         private StreamReader _streamReader;
         private Maze _maze;
+        private Char[,] _charArray;
+
 
         public void calculateSize() //Berekent de breedte en hoogte van het doolhof (MOET NOG GETEST WORDEN)
         {
@@ -28,10 +30,31 @@ namespace Sokoban
                 {
                     this._maze._width = line.Length;
                 }
-                
+                line = _streamReader.ReadLine();
             }
-
+            this._charArray = new Char[this._maze._width, this._maze._height];
 
         }
+
+        public void FillArray(int levelNumber) //vult de 2d array met alle tekens per regel
+        {
+            _path = "";
+            calculateSize();
+
+            String line = _streamReader.ReadLine();
+            int y = 0;
+
+            while (y < this._maze._height)
+            {
+                int x = 0;
+                foreach (Char c in line)
+                {
+                    this._charArray[x,y] = c;
+                    x++;
+                }
+                y++;
+                line = _streamReader.ReadLine();
+            }
         }
+    }
 }
